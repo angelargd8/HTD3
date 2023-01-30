2a27c8c59b4050317258ecb5e9a79150771cc9b1
angelargd8 <angelargd8@outlook.com>
2023-01-30 11:05:39 -0600

    creacion de clases

---
+++ src/CControlador/Controlador.java
@@ -0,0 +1,17 @@
+/*
+ * Autores:
+ *          Angela García #22869
+ *          Augusto Sanic #20717
+            Sergio Palacios #22808
+ * fecha de entrega:20/11/2022
+ * catedratico: Ludwing Cano
+ */
+package CControlador;
+import java.util.ArrayList;
+
+public class Controlador{
+  
+    
+
+    
+}
---
+++ src/CVista/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Autores:
+ *          Angela García #22869
+ * fecha de entrega: 
+ * catedratico:  
+ */
+package CVista;
+public class Main {
+    
+    /** 
+     * @param args
+     */
+    public static void main(String[] args) {
+        MostrarMenu mostrarMenu= new MostrarMenu();
+        mostrarMenu.MostrarPrograma();
+    }
+}
