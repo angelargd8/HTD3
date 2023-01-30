299cb593411e33b76ca39c0d0d82b699e226f31b
faguilarleal <agu22243@uvg.edu.gt>
2023-01-30 11:25:07 -0600

    Inicio de interfaz

--- src/CVista/Main.java
+++ src/CVista/Main.java
@@ -1,9 +1,13 @@
 /*
  * Autores:
  *          Angela García #22869
+ *          Francis Aguillar #22243
+ *          Gerardo Pineda #22880
+ *          Rodrigo Mansilla #22611
  * fecha de entrega: 
  * catedratico:  
  */
+
 package CVista;
 public class Main {
     
---
+++ src/Modelo/ISort.java
@@ -0,0 +1,6 @@
+package Modelo;
+
+public interface ISort {
+    public boolean compareTo(Object o);
+    public void sort(Comparable[] list);
+}
